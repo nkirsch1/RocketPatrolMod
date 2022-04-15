43aad1d9fc7024500eb7acb54ae07dfc9cb37295
nkirsch1 <103483876+nkirsch1@users.noreply.github.com>
2022-04-15 15:19:49 -0700

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+
+//Noah Kirsch, OH! THAT'S A BASEBALL, 4/15/22, 8 Hours to complete
+
+//Breakdown:
+//Allow the player to control the Rocket after it's fired [In Novice Mode] (5)
+//Create a new title screen (e.g., new artwork, typography, layout) (10)
+//Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+//Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
+//Create and implement a new weapon (w/ new behavior and graphics) [In Expert mode] (20)
+//Implement a simultaneous two-player mode [Press up to Play] (30)
+//Total: (105)
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
